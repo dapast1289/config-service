0f8bab5641afadd7f977a12b9ac2d5891d39e9ce
leochen <leo@funpodium.net>
2020-05-02 15:36:04 +0800

    first commit

---
+++ src/test/java/com/dapast1289/microservice/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dapast1289.microservice.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
